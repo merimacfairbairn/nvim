3bad6181dd04e47c8bf2797ffcef6c630bea51e8
Larry <eirikrinnrauthr@tutanota.com>
2024-01-05 18:23:58 +0000

    Add some netrw options

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -34,3 +34,7 @@ vim.api.nvim_create_autocmd({"BufWritePre"}, {
     pattern = '*',
     command = [[%s/\s\+$//e]],
 })
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
