a661be92a35bf438a9cdd1af77343a7585d2ab94
Larry <eirikrinnrauthr@tutanota.com>
2024-01-05 18:23:37 +0000

    Remove redundant comments

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -10,38 +10,29 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- Overwrite text when replacing with other text completely
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
--- Yank to clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
--- Delete to void register
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
--- Disable Q
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
--- Quickfix
 vim.keymap.set("n", "<C-t>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-h>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
--- Replace the current word
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
--- Make file executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
--- Write Source
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("w|so")
 end)
 
--- Run python
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "python",
     callback = function(args)
