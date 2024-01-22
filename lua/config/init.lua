f2c3dc76fcf1f886aa2b724b63e5f3928a66a9ee
Larry <eirikrinnrauthr@tutanota.com>
2024-01-22 17:34:08 +0000

    feat: Highlight yanked text briefly for clarity

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,3 +1,22 @@
 require("config.remap")
 require("config.set")
 require("config.status")
+
+-- autocmd's
+local autocmd = vim.api.nvim_create_autocmd
+autocmd({"BufWritePre"}, {
+    pattern = '*',
+    command = [[%s/\s\+$//e]],
+})
+
+local yankGroup = vim.api.nvim_create_augroup('HighLightYank', {})
+autocmd('TextYankPost', {
+    group = yankGroup,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
--- lua/config/set.lua
+++ lua/config/set.lua
@@ -30,11 +30,6 @@ vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
 
-vim.api.nvim_create_autocmd({"BufWritePre"}, {
-    pattern = '*',
-    command = [[%s/\s\+$//e]],
-})
-
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
