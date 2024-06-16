0fc26bf588922288f7e63e0794cc6d280d32e99e
Aboba <eirikrinnrauthr@tutanota.com>
2024-06-16 16:26:06 +0100

    Delete friendly snippets - they are crap. Install html snippets for web

--- lua/config/lazy/snippets.lua
+++ lua/config/lazy/snippets.lua
@@ -1,11 +1,11 @@
-return{
+return {
     "L3MON4D3/LuaSnip",
     -- follow latest release.
     version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
     -- install jsregexp (optional!).
     build = "make install_jsregexp",
 
-    dependencies = { "rafamadriz/friendly-snippets" },
+    dependencies = { "Wscats/html-snippets" },
 
     ft = {
         "go",
@@ -21,14 +21,14 @@ return{
         ls.filetype_extend("c", { "cdoc" })
         ls.filetype_extend("go", { "godoc" })
 
-        vim.keymap.set({"i", "s"}, "<C-j>", function() ls.jump(-1) end, {silent = true})
-        vim.keymap.set({"i", "s"}, "<C-k>", function() ls.jump( 1) end, {silent = true})
+        vim.keymap.set({ "i", "s" }, "<C-j>", function() ls.jump(-1) end, { silent = true })
+        vim.keymap.set({ "i", "s" }, "<C-k>", function() ls.jump(1) end, { silent = true })
 
-        vim.keymap.set({"i", "s"}, "<C-E>", function()
+        vim.keymap.set({ "i", "s" }, "<C-E>", function()
             if ls.choice_active() then
                 ls.change_choice(1)
             end
-        end, {silent = true})
+        end, { silent = true })
 
         require("luasnip.loaders.from_vscode").lazy_load()
     end,
