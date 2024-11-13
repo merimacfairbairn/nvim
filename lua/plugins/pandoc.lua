3fbf4fe1aab03e345be78812996daaf03ee933ef
Aboba <eirikrinnrauthr@tutanota.com>
2024-11-13 19:11:00 +0000

    feat: add pandoc integration

---
+++ lua/plugins/pandoc.lua
@@ -0,0 +1,12 @@
+return {
+        "vim-pandoc/vim-pandoc",
+        dependencies = {
+            "vim-pandoc/vim-pandoc-syntax",
+        },
+
+        ft = {
+            "pandoc",
+            "markdown",
+            "text",
+        },
+}
