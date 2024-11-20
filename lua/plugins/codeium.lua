6f3fa9381381a0a05139c75912d251d4a2010ccb
Aboba <eirikrinnrauthr@tutanota.com>
2024-11-20 20:59:59 +0000

    Change some keymaps in codeium config

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -3,8 +3,8 @@ return {
   event = "BufEnter",
   config = function ()
     vim.keymap.set('i', '<TAB>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
-    vim.keymap.set('i', '<C-r>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
-    vim.keymap.set('i', '<C-c>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
+    vim.keymap.set('i', '<C-l>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
+    vim.keymap.set('i', '<C-r>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
     vim.keymap.set('i', '<ESC>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
   end
 }
