8a7b0bbe08d0fe014fbbf536b714b707537916df
Aboba <eirikrinnrauthr@tutanota.com>
2025-03-13 23:14:52 +0000

    feat: try Glow instead of preview and render

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -131,27 +131,9 @@ return {
     },
     {
         'Kicamon/markdown-table-mode.nvim',
-        ft = {
-            "markdown",
-            "text",
-        },
         config = function()
             require('markdown-table-mode').setup()
         end
     },
-    {
-        "iamcco/markdown-preview.nvim",
-        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        ft = { "markdown" },
-        build = function() vim.fn["mkdp#util#install"]() end,
-    },
-    {
-        'MeanderingProgrammer/render-markdown.nvim',
-        dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-        ---@module 'render-markdown'
-        ---@type render.md.UserConfig
-        opts = {},
-    }
+    { "ellisonleao/glow.nvim", config = true, cmd = "Glow" },
 }
