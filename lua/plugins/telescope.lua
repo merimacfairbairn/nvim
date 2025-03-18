69dd6e1c3c8b704fcb42c577d68339042dad0561
Aboba <eirikrinnrauthr@tutanota.com>
2025-03-18 17:00:58 +0000

    perf(telescope): add fzf extension for better performance

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,8 @@
 return {
     "nvim-telescope/telescope.nvim",
     dependencies = {
-        "nvim-lua/plenary.nvim"
+        "nvim-lua/plenary.nvim",
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
     },
 
     config = function()
@@ -16,8 +17,19 @@ return {
                 grep_string = {
                     disable_devicons = true
                 },
+                extensions = {
+                    fzf = {
+                        fuzzy = true,                   -- false will only do exact matching
+                        override_generic_sorter = true, -- override the generic sorter
+                        override_file_sorter = true,    -- override the file sorter
+                        case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+                        -- the default case_mode is "smart_case"
+                    }
+                }
             },
         })
+
+        require("telescope").load_extension("fzf")
     end,
     keys = function()
         local builtin = require('telescope.builtin')
@@ -33,7 +45,7 @@ return {
             builtin.grep_string({ search = word });
         end)
         vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") });
+            builtin.live_grep();
         end)
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
     end,
