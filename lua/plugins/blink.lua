0350b414439ae5f181e6c6ca44badf710f002782
Aboba <eirikrinnrauthr@tutanota.com>
2025-03-18 22:04:57 +0000

    feat(blink): add a few more useful config lines

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,38 +1,42 @@
 return {
-  'saghen/blink.cmp',
-  dependencies = 'L3MON4D3/LuaSnip',
-  version = '*',
+    'saghen/blink.cmp',
+    dependencies = 'L3MON4D3/LuaSnip',
+    version = '*',
 
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+        keymap = {
+            preset = 'default',
 
-      keymap = {
-          preset = 'default',
+            ['<C-h>'] = { 'show_signature', 'hide_signature', 'fallback' },
+        },
 
-          ['<C-h>'] = { 'show_signature', 'hide_signature', 'fallback' },
-          ['K'] = { 'show', 'show_documentation', 'fallback' },
-      },
+        appearance = {
+            use_nvim_cmp_as_default = true,
+            nerd_font_variant = 'mono'
+        },
 
-    appearance = {
-      use_nvim_cmp_as_default = true,
-      nerd_font_variant = 'mono'
+        sources = {
+            default = { 'lsp', 'path', 'snippets', 'buffer' },
+        },
+
+        fuzzy = { implementation = "prefer_rust_with_warning" },
+
+        snippets = { preset = "luasnip" },
+
+        completion = {
+            keyword = { range = 'full' },
+
+            accept = { auto_brackets = { enabled = false }, },
+
+            documentation = { auto_show = true, auto_show_delay_ms = 500 },
+
+            ghost_text = { enabled = true },
+        },
+
+        signature = { enabled = true },
     },
 
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-    },
-
-    fuzzy = { implementation = "prefer_rust_with_warning" },
-
-    snippets = { preset = "luasnip" },
-
-    completion = {
-        accept = { auto_brackets = { enabled = false }, },
-    },
-
-    signature = { enabled = true },
-  },
-
-  opts_extend = { "sources.default" }
+    opts_extend = { "sources.default" }
 }
