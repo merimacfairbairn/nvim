4bfad70460252b5503dfcce4c6d3af6d60f7c641
Aboba <eirikrinnrauthr@tutanota.com>
2025-03-19 21:51:00 +0000

    fix(telescope): remove keymaps for word and WORD under cursor
    In favour of builtin telescope ones

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -36,17 +36,10 @@ return {
     end,
     keys = function()
         local builtin = require('telescope.builtin')
+        local cursor = require('telescope.themes').get_cursor()
 
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>pws', function()
-            local word = vim.fn.expand("<cword>")
-            builtin.grep_string({ search = word });
-        end)
-        vim.keymap.set('n', '<leader>pWs', function()
-            local word = vim.fn.expand("<cWORD>")
-            builtin.grep_string({ search = word });
-        end)
         vim.keymap.set('n', '<leader>ps', function()
             builtin.live_grep();
         end)
