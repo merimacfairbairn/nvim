2deea6a473c807bd1b2f6ba3bd3d817db79742d9
Aboba <eirikrinnrauthr@tutanota.com>
2025-03-20 16:47:28 +0000

    fix(blink): disable ghost text for blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -31,8 +31,6 @@ return {
             accept = { auto_brackets = { enabled = false }, },
 
             documentation = { auto_show = true, auto_show_delay_ms = 500 },
-
-            ghost_text = { enabled = true },
         },
 
         signature = { enabled = true },
