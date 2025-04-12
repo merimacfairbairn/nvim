f2d31f9c876fa6693590eb07f5d13b5a6ff260ad
Aboba <eirikrinnrauthr@tutanota.com>
2025-04-12 17:49:39 +0100

    fix(oil): remove conflicting keymaps

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,6 +1,6 @@
 return {
   'stevearc/oil.nvim',
-  dependencies = { "nvim-tree/nvim-web-devicons" },   -- use if you prefer nvim-web-devicons
+  dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
   ---@module 'oil'
   ---@type oil.SetupOpts
   opts = {
@@ -8,6 +8,21 @@ return {
 
     lsp_file_methods = {
       autosave_changes = true,
+    },
+
+    use_default_keymaps = false,
+    keymaps = {
+      ["g?"] = { "actions.show_help", mode = "n" },
+      ["<CR>"] = "actions.select",
+      ["<C-c>"] = { "actions.close", mode = "n" },
+      ["<C-l>"] = "actions.refresh",
+      ["-"] = { "actions.parent", mode = "n" },
+      ["_"] = { "actions.open_cwd", mode = "n" },
+      ["`"] = { "actions.cd", mode = "n" },
+      ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+      ["gs"] = { "actions.change_sort", mode = "n" },
+      ["gx"] = "actions.open_external",
+      ["g."] = { "actions.toggle_hidden", mode = "n" },
     },
   },
 
