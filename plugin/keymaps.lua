7c00b8bc0f4ef787c028c48481747c51fca15d42
Aboba <eirikrinnrauthr@tutanota.com>
2025-05-05 13:59:27 +0100

    fix(keymaps): delete put keymaps since they are bad for single chars

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -9,18 +9,6 @@ set("n", "<C-u>", "<C-u>zz")
 set("n", "n", "nzzzv")
 set("n", "N", "Nzzzv")
 
-set("n", "p", function()
-  local row, cursor = unpack(vim.api.nvim_win_get_cursor(0))
-  vim.cmd("put")
-  vim.api.nvim_win_set_cursor(0, { row + 1, cursor })
-end)
-
-set("n", "P", function()
-  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-  vim.cmd("normal! P")
-  vim.api.nvim_win_set_cursor(0, { row, col })
-end)
-
 set("x", "<space>p", [["_dP]])
 
 set({ "n", "v" }, "<space>y", [["+y]])
@@ -35,6 +23,8 @@ set("n", "<C-k>", "<cmd>cnext<CR>zz")
 set("n", "<C-j>", "<cmd>cprev<CR>zz")
 set("n", "<space>k", "<cmd>lnext<CR>zz")
 set("n", "<space>j", "<cmd>lprev<CR>zz")
+
+set("n", "<leader>t", "<cmd>PlenaryBustedFile %<CR>")
 
 set("i", "<C-c>", "<Esc>")
 
