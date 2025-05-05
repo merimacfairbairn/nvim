b3f5b3aa815e8531495651c56c308cdf14df3c4b
Aboba <eirikrinnrauthr@tutanota.com>
2025-05-05 13:59:03 +0100

    Set up keybindings for autoformat

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -1,9 +1,22 @@
 return {
   dir = "~/projects/plugins/autoformat.nvim/",
   name = "autoformat",
+  branch = "main",
   lazy = false,
   config = function()
-    require("autoformat").setup()
-    vim.keymap.set("n", "<Leader>tf", "<cmd>AutoformatToggleFile<cr>")
+    require("autoformat").setup({
+      always_block = {
+        files = {},
+        filetypes = { "htmldjango" },
+        projects = {},
+      },
+    })
+
+    local af = require("autoformat")
+    local ui = require("autoformat.ui")
+    vim.keymap.set("n", "<Leader>af", ui.toggle_menu, { noremap = true, silent = true })
+    vim.keymap.set("n", "<Leader>fa", af.toggle_file, { noremap = true, silent = true })
+    vim.keymap.set("n", "<Leader>ft", af.toggle_ft, { noremap = true, silent = true })
+    vim.keymap.set("n", "<Leader>fs", af.toggle_project, { noremap = true, silent = true })
   end
 }
