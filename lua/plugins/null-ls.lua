baef348e1ac506532f1ef3800853f4dd214072ae
Aboba <eirikrinnrauthr@tutanota.com>
2025-05-18 14:49:02 +0100

    feat(null-ls): format python with black

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -8,15 +8,15 @@ return {
 
   config = function()
     require("mason-null-ls").setup({
-      ensure_installed = { "isort", "djlint" },
+      ensure_installed = { "isort", "black", "djlint" },
       automatic_installation = true,
     })
 
     local null_ls = require("null-ls")
-
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.isort,
+        null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.djlint.with({
           filetypes = { "htmldjango" },
           extra_args = { "--indent-size", "2" },
