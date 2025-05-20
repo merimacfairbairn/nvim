39b9fd69bc2b417da1801b70d6d5b135f2007112
Aboba <eirikrinnrauthr@tutanota.com>
2025-05-20 18:59:29 +0100

    fix(images): only render image under cursor

--- lua/plugins/images.lua
+++ lua/plugins/images.lua
@@ -1,5 +1,12 @@
 return {
   "3rd/image.nvim",
   ft = { "markdown", "vimwiki", "css", "html", "htmldjango" },
-  opts = {},
+  opts = {
+    integrations = {
+      markdown = {
+        only_render_image_at_cursor = true,
+        only_render_image_at_cursor_mode = "inline",
+      },
+    },
+  },
 }
